9af1aeeb7a53720e377275a89a4aa2d157be565f
YourDailyMoose <90841132+YourDailyMoose@users.noreply.github.com>
2023-11-04 15:32:56 +1100

    Add path-browserify package and remove whitespace in index.js

---
+++ dashboard/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify")
+      
+    }
+  }
+};
