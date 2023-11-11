ea26c7062064c4ae5f930138366175348a0675bd
YourDailyMoose <90841132+YourDailyMoose@users.noreply.github.com>
2023-11-11 23:45:59 +1100

    Update navbar links and add login button

--- dashboard/src/pages/dashboard/dashboard.js
+++ dashboard/src/pages/dashboard/dashboard.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import './dashboard.css';
 
-const Dashboard = (//props
-) => {
-    
-        <div>
-            <nav className="navbar">
-                <div className="navbar-brand">Scout</div>
-                <ul className="navbar-nav">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About Us</a></li>
-                    <li><a href="/invite">Invite</a></li>
-                    <li><a href="/support">Support Server</a></li>
-                </ul>
-                <h4 class='loggedinuser'>Logged in as USER</h4>
-            </nav>
-        </div>
-     
-};
+const About = () => (
+  <div>
+    <nav className="navbar">
+      <div className="navbar-brand">Scout</div>
+      <ul className="navbar-nav">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about-us">About Us</a></li>
+        <li><a href="/invite">Invite</a></li>
+        <li><a href="/support">Support Server</a></li>
+      </ul>
+      <a href="https://discord.com/api/oauth2/authorize?client_id=1157304018939424970&redirect_uri=https%3A%2F%2Fscoutbot.me%2Fapi%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify%20guilds%20email" class="loginbutton" role="button">Log In</a>
+    </nav>
+  </div>
+);
 
 export default Dashboard;
