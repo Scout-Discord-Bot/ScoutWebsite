b701b68938685c9c463f6d3f1e39c40762b64381
YourDailyMoose <90841132+YourDailyMoose@users.noreply.github.com>
2023-11-11 21:27:16 +1100

    Update asset-manifest and main.js for ScoutBot dashboard

--- dashboard/src/pages/dashboard.js
+++ dashboard/src/pages/dashboard.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './dashboard/dashboard.css';
 
-const Dashboard = (props) => {
-    return (
-        <div>
-            <nav className="navbar">
-                <div className="navbar-brand">Scout</div>
-                <ul className="navbar-nav">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About Us</a></li>
-                    <li><a href="/invite">Invite</a></li>
-                    <li><a href="/support">Support Server</a></li>
-                </ul>
-                <h4 class='loggedinuser'>Logged in as {props.user.username}</h4>
-            </nav>
-        </div>
-    );
+const Dashboard = (//props
+) => {
+    <div>
+        <nav className="navbar">
+            <div className="navbar-brand">Scout</div>
+            <ul className="navbar-nav">
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About Us</a></li>
+                <li><a href="/invite">Invite</a></li>
+                <li><a href="/support">Support Server</a></li>
+            </ul>
+            <h4 class='loggedinuser'>Logged in as USER</h4>
+        </nav>
+    </div>
+         // {props.user.username}
 };
 
 export default Dashboard;
