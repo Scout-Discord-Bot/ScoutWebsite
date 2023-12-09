1a01e4a4826102376987b4e59ed82752869a7994
YourDailyMoose <90841132+YourDailyMoose@users.noreply.github.com>
2023-12-09 13:32:57 +1100

    Update login button URL

--- dashboard/src/components/navigation.js
+++ dashboard/src/components/navigation.js
@@ -16,7 +16,7 @@ const Navigation = ({ isAuthenticated, userData, onLogout }) => (
         {isAuthenticated ? (
             <ProfileDropdown userData={userData} onLogout={onLogout} />
         ) : (
-            <a href="https://discord.com/api/oauth2/authorize?client_id=1157304018939424970&redirect_uri=https%3A%2F%2Fapi.scoutbot.me%2Fapi%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify%20guilds%20email" className="loginbutton" role="button">Log In</a>
+            <a href="https://discord.gg/invite/BwD7MgVMuq" className="loginbutton" role="button">Get Updates</a>
         )}
     </nav>
 );
