64feeaa2828341fc77e7da70f70f949f4a7e29c5
YourDailyMoose <90841132+YourDailyMoose@users.noreply.github.com>
2024-02-09 12:31:00 +1100

    Update login and support server URLs, remove privacy policy file, and update about page

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './about.css';
+import Navigation from '../../components/navigation';
+import Footer from '../../components/footer';
 
-const About = () => (
-  <div>
-    <nav className="navbar">
-      <div className="navbar-brand">Scout</div>
-      <ul className="navbar-nav">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about-us">About Us</a></li>
-        <li><a href="/invite">Invite</a></li>
-        <li><a href="/support">Support Server</a></li>
-      </ul>
-      <a href="https://discord.com/api/oauth2/authorize?client_id=1157304018939424970&redirect_uri=https%3A%2F%2Fscoutbot.me%2Fapi%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify%20guilds%20email" class="loginbutton" role="button">Log In</a>
-    </nav>
-  </div>
-);
-
-export default About;
+const About = () => {
+  
+    return (
+      <div>
+        <Navigation></Navigation>
+        <div className="about">
+          <h1>About Us</h1>
+          <p>Welcome to the about us page. More Coming Soon!</p>
+        </div>
+        <Footer></Footer>
+      </div>
+    );
+  };
+  
+  export default About;
