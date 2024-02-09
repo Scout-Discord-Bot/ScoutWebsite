ea2afab7dfcd1b6ce43f120cf02f4cd648c8d049
YourDailyMoose <90841132+YourDailyMoose@users.noreply.github.com>
2024-02-09 12:14:03 +1100

    Add login and support server redirects, remove support page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home';
 import About from './pages/about/about';
-import Support from './pages/support/support';
 import Dashboard from './pages/dashboard/dashboard';
 import './App.css';
 
@@ -11,7 +10,6 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
-      <Route path="/support" element={<Support/>} />
       <Route path="/dashboard" element={<Dashboard/>} />
     </Routes>
   </Router>
--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,18 @@ const Footer = () => (
 
 
     <div class="footer">
+
+
+        <ul class="options">
+            <li><a href="/legal/privacy-policy">Privacy Policy</a></li>
+            <li><a href="/legal/terms-of-service">Terms of Service</a></li>
+            <li><a href="/support">Support Server</a></li>
+        </ul>
+
+        <p>Scout is not affiliated with Discord Inc. All rights reserved. - Discord, Discord API, and Discord Bot are registered trademarks of Discord Inc.</p>
+
+
+
         <p>© Scout 2023-2024 | Created by YourDailyMoose | Contributors: Limitless4315 & 1spinnewiel</p>
     </div>
 
